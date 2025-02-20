acf3c37cc14fe437ea646a52e0d28d0b1c5695f3
Екатерина Швец <mad-et@outlook.com>
2025-02-20 15:19:10 +0300

    fix

--- HomeWorks/Program.cs
+++ HomeWorks/Program.cs
@@ -2,3 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
